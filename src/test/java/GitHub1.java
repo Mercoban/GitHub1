5f66082d77be7cd6bb39c06d2f2e364fd74fd8b0
Murat <eposta@site.com>
2021-12-10 09:30:08 +0300

    1.master

--- src/test/java/GitHub1.java
+++ src/test/java/GitHub1.java
@@ -3,7 +3,7 @@ public class GitHub1 {
     public static void main(String[] args) {
         System.out.println("Merhaba Dünya");
         System.out.println("murat local");
-
+        System.out.println("1.master ");
     }
 
 }
